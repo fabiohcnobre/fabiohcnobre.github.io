657efd3b652073c79616e791c012af4ab08f4a53
Fabio Nobre <fabiohcnobre@hotmail.com>
2024-04-02 22:32:12 -0300

    #0 Dark Mode

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,3 +8,23 @@ image: { src: "img/github.svg", width: 100, height: 100 } }, opacity: { value: .
        { detect_on: "canvas", events: { onhover: { enable: !1, mode: "grab" }, onclick: { enable: !0, mode: "push" }, resize: !0 }, modes:
         { grab: { distance: 400, line_linked: { opacity: 1 } }, bubble: { distance: 400, size: 40, duration: 2, opacity: 8, speed: 3 },
          repulse: { distance: 200, duration: .4 }, push: { particles_nb: 4 }, remove: { particles_nb: 2 } } }, retina_detect: !0 }) }, !1);
+
+         function toggleTheme(){
+          if (document.documentElement.getAttribute('data-bs-theme') == 'dark') {
+              document.documentElement.setAttribute('data-bs-theme', 'light')
+          }
+          else {
+              document.documentElement.setAttribute('data-bs-theme', 'dark')
+          }
+          var element = document.querySelector('.fas.fa-solid.fa-moon'); 
+          if(element == null)
+            element = document.querySelector('.fas.fa-solid.fa-sun');
+          if (element.classList.contains('fa-moon')) {
+              element.classList.remove('fa-moon');
+              element.classList.add('fa-sun');
+          } else {
+              element.classList.remove('fa-sun');
+              element.classList.add('fa-moon');
+          }
+          
+        }
